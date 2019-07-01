3982c550ea57a6daad57c0f2bb4ed204610e45e9
ftevxk <ftevxk@gmail.com>
2019-07-01 14:12:27 +0800

    DataBinding增加替换统一布局资源方法

--- library/src/main/java/com/ftevxk/core/extension/RecyclerViewExtension.kt
+++ library/src/main/java/com/ftevxk/core/extension/RecyclerViewExtension.kt
@@ -18,10 +18,141 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ftevxk.core.common.DataBindAdapter
 import com.ftevxk.core.common.IDataBindItemModel
 
-/****************************************
+/*****************************************************************
+ * 点击事件相关
+ * 可用于任意RecyclerView.Adapter，与DataBindAdapter无关
+ *****************************************************************/
+
+/**
+ * 扩展RecyclerView的点击事件
+ */
+fun RecyclerView.setOnItemClickListener(
+    onItemClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null) {
+    var itemGesture = getTag("ItemGesture".hashCode()) as? ItemGesture
+    if (itemGesture == null) {
+        itemGesture = ItemGesture(this)
+        setTag("ItemGesture".hashCode(), itemGesture)
+    }
+    itemGesture.onItemClickListener = onItemClickListener
+}
+
+/**
+ * 扩展RecyclerView的长按事件
+ */
+fun RecyclerView.setOnItemLongClickListener(
+    onItemLongClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null) {
+    var itemGesture = getTag("ItemGesture".hashCode()) as? ItemGesture
+    if (itemGesture == null) {
+        itemGesture = ItemGesture(this)
+        setTag("ItemGesture".hashCode(), itemGesture)
+    }
+    itemGesture.onItemLongClickListener = onItemLongClickListener
+}
+
+/**
+ * item点击的位置是否为指定控件
+ */
+fun RecyclerView.ViewHolder.isClickControlView(view: View?): Boolean {
+    if (view == null) return false
+    val recyclerView = clickRecyclerView
+    val clickLocation = clickLocation
+    if (recyclerView == null || clickLocation == null) return false
+    val recyclerRect = Rect()
+    recyclerView.getGlobalVisibleRect(recyclerRect)
+    val viewRect = Rect()
+    view.getGlobalVisibleRect(viewRect)
+    if (clickLocation[0] >= viewRect.left - recyclerRect.left &&
+        clickLocation[0] <= viewRect.right - recyclerRect.left &&
+        clickLocation[1] >= viewRect.top - recyclerRect.top &&
+        clickLocation[1] <= viewRect.bottom - recyclerRect.top
+    ) {
+        return true
+    }
+    return false
+}
+
+/**
+ * Item点击时存取坐标位置
+ */
+var RecyclerView.ViewHolder.clickLocation: Array<Float>?
+    get() = itemView.getTag("clickLocation".hashCode()) as? Array<Float>
+    set(value) = itemView.setTag("clickLocation".hashCode(), value)
+
+/**
+ * Item点击时临时存取RecyclerView对象
+ */
+private var RecyclerView.ViewHolder.clickRecyclerView: RecyclerView?
+    get() = itemView.getTag("clickRecyclerView".hashCode()) as? RecyclerView
+    set(value) = itemView.setTag("clickRecyclerView".hashCode(), value)
+
+/**
+ * 手势事件封装类
+ */
+open class ItemGesture(private val recyclerView: RecyclerView) :
+    GestureDetector.SimpleOnGestureListener(), RecyclerView.OnItemTouchListener {
+
+    init {
+        recyclerView.addOnItemTouchListener(this)
+    }
+
+    var onItemClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null
+
+    var onItemLongClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null
+
+    private var detector = GestureDetectorCompat(recyclerView.context, this)
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        detector.onTouchEvent(e)
+        return false
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+
+    /**
+     * 点击
+     */
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        val view = recyclerView.findChildViewUnder(e.x, e.y)
+        if (view != null) {
+            val position = recyclerView.getChildLayoutPosition(view)
+            val holder =
+                recyclerView.findViewHolderForLayoutPosition(position) as RecyclerView.ViewHolder
+            holder.clickRecyclerView = recyclerView
+            holder.clickLocation = arrayOf(e.x, e.y)
+            view.performClick()
+            onItemClickListener?.invoke(holder, position)
+            return true
+        }
+        return false
+    }
+
+    /**
+     * 长按
+     */
+    override fun onLongPress(e: MotionEvent) {
+        val view = recyclerView.findChildViewUnder(e.x, e.y)
+        if (view != null) {
+            val position = recyclerView.getChildLayoutPosition(view)
+            val holder =
+                recyclerView.findViewHolderForLayoutPosition(position) as RecyclerView.ViewHolder
+            holder.clickRecyclerView = recyclerView
+            holder.clickLocation = arrayOf(e.x, e.y)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                view.performLongClick(e.x, e.y)
+            } else {
+                view.performLongClick()
+            }
+            onItemLongClickListener?.invoke(holder, position)
+        }
+    }
+}
+
+/*****************************************************************
  * RecyclerView相关扩展
  * 部分扩展涉及DataBindAdapter
- ***************************************/
+ *****************************************************************/
 
 /**
  * 获得item的ViewHolder
@@ -44,7 +175,7 @@ fun <VDB : ViewDataBinding> RecyclerView.ViewHolder.getItemBinding(): VDB? {
     return (this as? DataBindAdapter.ViewHolder)?.binding as? VDB
 }
 
-/******************************************************************
+/*****************************************************************
  * 如果adapter为DataBindAdapter则提供相应数据操作方法
  *****************************************************************/
 
@@ -126,135 +257,4 @@ fun <T : IDataBindItemModel> RecyclerView.ViewHolder.getItemModel(position: Int)
 
 fun RecyclerView.resetLayoutRes(@LayoutRes layoutRes: Int?, vararg exclude: Int) {
     getDataBindAdapter()?.resetLayoutRes(layoutRes, *exclude)
-}
-
-/*****************************************************************
- * 点击事件相关
- * 可用于任意RecyclerView.Adapter，与DataBindAdapter无关
- *****************************************************************/
-
-/**
- * 扩展RecyclerView的点击事件
- */
-fun RecyclerView.setOnItemClickListener(
-        onItemClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null) {
-    var itemGesture = getTag("ItemGesture".hashCode()) as? ItemGesture
-    if (itemGesture == null) {
-        itemGesture = ItemGesture(this)
-        setTag("ItemGesture".hashCode(), itemGesture)
-    }
-    itemGesture.onItemClickListener = onItemClickListener
-}
-
-/**
- * 扩展RecyclerView的长按事件
- */
-fun RecyclerView.setOnItemLongClickListener(
-        onItemLongClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null) {
-    var itemGesture = getTag("ItemGesture".hashCode()) as? ItemGesture
-    if (itemGesture == null) {
-        itemGesture = ItemGesture(this)
-        setTag("ItemGesture".hashCode(), itemGesture)
-    }
-    itemGesture.onItemLongClickListener = onItemLongClickListener
-}
-
-/**
- * item点击的位置是否为指定控件
- */
-fun RecyclerView.ViewHolder.isClickControlView(view: View?): Boolean {
-    if (view == null) return false
-    val recyclerView = clickRecyclerView
-    val clickLocation = clickLocation
-    if (recyclerView == null || clickLocation == null) return false
-    val recyclerRect = Rect()
-    recyclerView.getGlobalVisibleRect(recyclerRect)
-    val viewRect = Rect()
-    view.getGlobalVisibleRect(viewRect)
-    if (clickLocation[0] >= viewRect.left - recyclerRect.left &&
-            clickLocation[0] <= viewRect.right - recyclerRect.left &&
-            clickLocation[1] >= viewRect.top - recyclerRect.top &&
-            clickLocation[1] <= viewRect.bottom - recyclerRect.top
-    ) {
-        return true
-    }
-    return false
-}
-
-/**
- * Item点击时存取坐标位置
- */
-var RecyclerView.ViewHolder.clickLocation: Array<Float>?
-    get() = itemView.getTag("clickLocation".hashCode()) as? Array<Float>
-    set(value) = itemView.setTag("clickLocation".hashCode(), value)
-
-/**
- * Item点击时临时存取RecyclerView对象
- */
-private var RecyclerView.ViewHolder.clickRecyclerView: RecyclerView?
-    get() = itemView.getTag("clickRecyclerView".hashCode()) as? RecyclerView
-    set(value) = itemView.setTag("clickRecyclerView".hashCode(), value)
-
-/**
- * 手势事件封装类
- */
-open class ItemGesture(private val recyclerView: RecyclerView) :
-        GestureDetector.SimpleOnGestureListener(), RecyclerView.OnItemTouchListener {
-
-    init {
-        recyclerView.addOnItemTouchListener(this)
-    }
-
-    var onItemClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null
-
-    var onItemLongClickListener: ((holder: RecyclerView.ViewHolder, position: Int) -> Unit)? = null
-
-    private var detector = GestureDetectorCompat(recyclerView.context, this)
-
-    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
-
-    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-        detector.onTouchEvent(e)
-        return false
-    }
-
-    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
-
-    /**
-     * 点击
-     */
-    override fun onSingleTapUp(e: MotionEvent): Boolean {
-        val view = recyclerView.findChildViewUnder(e.x, e.y)
-        if (view != null) {
-            val position = recyclerView.getChildLayoutPosition(view)
-            val holder =
-                    recyclerView.findViewHolderForLayoutPosition(position) as RecyclerView.ViewHolder
-            holder.clickRecyclerView = recyclerView
-            holder.clickLocation = arrayOf(e.x, e.y)
-            view.performClick()
-            onItemClickListener?.invoke(holder, position)
-            return true
-        }
-        return false
-    }
-
-    /**
-     * 长按
-     */
-    override fun onLongPress(e: MotionEvent) {
-        val view = recyclerView.findChildViewUnder(e.x, e.y)
-        if (view != null) {
-            val position = recyclerView.getChildLayoutPosition(view)
-            val holder =
-                    recyclerView.findViewHolderForLayoutPosition(position) as RecyclerView.ViewHolder
-            holder.clickRecyclerView = recyclerView
-            holder.clickLocation = arrayOf(e.x, e.y)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                view.performLongClick(e.x, e.y)
-            } else {
-                view.performLongClick()
-            }
-            onItemLongClickListener?.invoke(holder, position)
-        }
-    }
 }
