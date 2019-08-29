0a470af3eb4ca712fd868a8a57fa4e31bef4f72c
ftevxk <ftevxk@gmail.com>
2019-08-29 15:55:35 +0800

    增加startActivityForResult直接处理返回数据的扩展方法

---
+++ library/src/main/java/com/ftevxk/core/extension/ActivityExtension.kt
@@ -0,0 +1,34 @@
+@file:Suppress("UNUSED_VARIABLE", "unused")
+
+package com.ftevxk.core.extension
+
+import android.app.Activity
+import android.content.Intent
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.ftevxk.core.fragment.ResultFragment
+import org.jetbrains.anko.internals.AnkoInternals
+
+inline fun <reified T: Activity> FragmentActivity.startActivityForResult(vararg params: Pair<String, Any?>, noinline result: (Intent?) -> Unit){
+    val intent = AnkoInternals.createIntent(this, T::class.java, params)
+    val fragment = ResultFragment(intent){ fragment, it ->
+        result.invoke(it)
+        supportFragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss()
+    }
+    supportFragmentManager.beginTransaction().add(0, fragment).commitAllowingStateLoss()
+}
+
+inline fun <reified T: Activity> Fragment.startActivityForResult(vararg params: Pair<String, Any?>, noinline result: (Intent?) -> Unit){
+    val intent = AnkoInternals.createIntent(activity!!, T::class.java, params)
+    val fragment = ResultFragment(intent){ fragment, it ->
+        result.invoke(it)
+        childFragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss()
+    }
+    childFragmentManager.beginTransaction().add(0, fragment).commitAllowingStateLoss()
+}
+
+fun FragmentActivity.finish(vararg params: Pair<String, Any?>){
+    val intent = AnkoInternals.createIntent(this, javaClass, params)
+    setResult(Activity.RESULT_OK, intent)
+    finish()
+}
