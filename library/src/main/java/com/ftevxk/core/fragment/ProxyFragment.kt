cda6a93098a5c8dd7e69c2cd6b9c951a8b642d65
ftevxk <ftevxk@gmail.com>
2019-10-24 16:05:30 +0800

    ProxyFragment设置retainInstance

--- library/src/main/java/com/ftevxk/core/fragment/ProxyFragment.kt
+++ library/src/main/java/com/ftevxk/core/fragment/ProxyFragment.kt
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -56,6 +57,11 @@ private constructor(private val attach: (ProxyFragment, Int) -> Unit,
             fragmentManager.beginTransaction().add(0, fragment).commitAllowingStateLoss()
             return fragment
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
     }
 
     override fun onAttach(context: Context) {
